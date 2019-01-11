e36f6747660b659d09125cf6044248289909ed95
SynronGroup <34838141+synron@users.noreply.github.com>
2019-01-11 18:57:07 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,3 +10,6 @@ echo ${CI_PROJECT_NAME}:${CI_COMMIT_REF_NAME}
 wget -q -O gitlab_ci https://raw.githubusercontent.com/synron/gitlab_ci/master/mvn.sh
 chmod +x gitlab_ci
 \mv -f gitlab_ci /usr/local/bin/gitlab_ci
+
+
+
