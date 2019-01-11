2c3cea881bc2ffa437fb777c79b1d7fe89692d85
SynronGroup <34838141+synron@users.noreply.github.com>
2019-01-12 00:29:32 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 set -eu
 
+echo "查看系统变量"
 echo ${CI_PROJECT_URL}
 echo ${CI_PROJECT_DIR}
 echo ${CI_PROJECT_NAME}:${CI_COMMIT_REF_NAME}
 
 
-
+echo "初始化构建脚本"
 wget -q -O gitlab_ci https://raw.githubusercontent.com/synron/gitlab_ci/master/deployer.sh
 chmod +x gitlab_ci
 \mv -f gitlab_ci /usr/local/bin/gitlab_ci
