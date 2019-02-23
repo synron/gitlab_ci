bd1622c36d8afe5bf72f80a2076626bb186bb985
SynronGroup <34838141+synron@users.noreply.github.com>
2019-02-23 21:23:05 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ echo ${CI_PROJECT_NAME}:${CI_COMMIT_REF_NAME}
 
 
 echo "初始化构建脚本"
-wget -q -O gitlab_ci https://raw.githubusercontent.com/synron/gitlab_ci/master/deployer.sh
+wget -q -O gitlab_ci https://raw.githubusercontent.com/synron/gitlab_ci/master/deployer_mvn.sh
 chmod +x gitlab_ci
 \mv -f gitlab_ci /usr/local/bin/gitlab_ci
 
